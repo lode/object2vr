6fffbbb6281130f6bf8d38fa7348f452a5635c8c
Lode Claassen <lode@alsvanzelf.nl>
2016-12-01 21:57:51 +0100

    adds first script and installation instructions

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # object2vr
-Loading 360 photos made with object2vr, specifically for Lightspeed eCommerce.
+
+Loading 360 photos made with [object2vr](https://ggnome.com/object2vr), specifically for [Lightspeed eCommerce](https://www.lightspeedhq.nl/ecommerce/).
+
+This loads 360 photos over the first image in the gallery of a Lightspeed product page.
+
+
+## Install
+
+1. Download object2vr and place it on your server.
+2. Upload the object2vr files (containing `TDMS_out.xml` and `images/`) to your server.
+3. Load the object2vr script on the Lightspeed product page
+
+``` html
+<script type="text/javascript" src="//example.com/object2vr.js"></script>
+<script type="text/javascript">
+	object2vr({
+		locationBase: '//example.com/object2vr' + window.location.pathname,
+		mimicElement: jQuery('.zoombox .images .zoom.first'),
+	});
+</script>
+```
+
+Note: the example uses the pathname of the current page as an identifier for finding the object2vr files.
+This is an easy way to connect products to an object2vr image. However, other ways of doing this are fine as well.
+
+
+## Contribute
+
+Pull Requests or issues are welcome!
+
+Some ideas:
+
+- Converting to a generic script working for other CMS environments.
+- Making it independent of jQuery.
+
+
+## License
+
+[MIT](/LICENSE)
---
+++ object2vr.js
@@ -0,0 +1,108 @@
+;(function($){
+'use strict';
+	/**
+	 * load object2vr 360 photos
+	 * 
+	 * @see https://ggnome.com/object2vr
+	 * 
+	 * @param  {object} options:
+	 * - locationBase: path where all 360 photos are located
+	 *                 this can be absolute/relative and external/local
+	 *                 @note include trailing slash
+	 * - mimicElement: overlay the object2vr over this element
+	 *                 its properties are taken and the 360 is display on top
+	 *                 @note must be a jQuery object
+	 */
+	window.object2vr = function(options) {
+		if (options.length < 1 || options.locationBase == undefined || options.mimicElement == undefined) {
+			console.notice('object2vr: missing options, provide locationBase and mimicElement');
+			return;
+		}
+		
+		if (options.mimicElement.length < 1) {
+			console.notice('object2vr: can\'t find mimicElement, note it should be a jQuery object');
+			return;
+		}
+		
+		bootstrap(options);
+	}
+	
+	/**
+	 * private below
+	 */
+	
+	var locationBase  = '';
+	var mimicElement  = undefined;
+	var firstPhoto    = undefined;
+	var container     = undefined;
+	var loadedScripts = 0;
+	
+	/**
+	 * handle events
+	 */
+	jQuery('.zoombox').on('mousedown', '.thumbs a', function(event){ switchImages(event.target); });
+	
+	/**
+	 * bootstraps and loads the object2vr
+	 */
+	function bootstrap(options) {
+		locationBase = options.locationBase;
+		mimicElement = options.mimicElement;
+		
+		prepareImageSwitching();
+		prepareContainer();
+		
+		jQuery.getScript(locationBase + '/object2vr_player.js', load);
+		jQuery.getScript(locationBase + '/skin.js', load);
+	}
+	
+	/**
+	 * finds the first photo in the gallery as that one needs special treatment
+	 */
+	function prepareImageSwitching() {
+		firstPhoto = jQuery('.zoombox .thumbs > *:first img')[0]
+	}
+	
+	/**
+	 * mimic the styling of another element
+	 */
+	function prepareContainer() {
+		container = jQuery('<div id="object2vr-container-360"></div>');
+		
+		container.css('display',  'none');
+		container.css('position', 'absolute');
+		container.css('z-index',  10);
+		container.css('top',      mimicElement.offset().top);
+		container.css('left',     mimicElement.offset().left);
+		container.css('width',    mimicElement.width());
+		container.css('height',   mimicElement.height());
+		
+		jQuery('body').append(container);
+	}
+	
+	/**
+	 * load the object2vr xml and show the 360 photo
+	 */
+	function load() {
+		// wait for all scripts to load
+		loadedScripts += 1;
+		if (loadedScripts < 2) {
+			return;
+		}
+		
+		var player = new object2vrPlayer(container.attr('id'));
+		var skin   = new object2vrSkin(player, locationBase);
+		player.readConfigUrl(locationBase + 'TDMS_out.xml');
+		
+		container.css('display', 'block');
+	}
+	
+	/**
+	 * hides the object2vr as other images are clicked
+	 * or shows when the first placeholder image is click
+	 */
+	function switchImages(clickedImage) {
+		var newDisplay = (clickedImage == firstPhoto) ? 'block' : 'none';
+		container.css('display', newDisplay);
+	};
+})(jQuery);
